4705a33d6c1672a56fa76d95f13940678b17e1ca
diegof <diegolimon123@gmail.com>
2020-12-01 01:36:20 -0300

    e Please entee the commit message for your changes. Lines starting

--- fuentes/app/src/main/java/cl/inacap/thesimpsons/adapters/PersonajesAdapter.java
+++ fuentes/app/src/main/java/cl/inacap/thesimpsons/adapters/PersonajesAdapter.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
 import cl.inacap.thesimpsons.R;
@@ -30,16 +32,17 @@ public class PersonajesAdapter extends ArrayAdapter<Personaje> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
         LayoutInflater inflater = this.activity.getLayoutInflater();
-        View rowView = inflater.inflate((R.layout.personajes_list,null,true));
+        View rowView = inflater.inflate(R.layout.personajes_list,null,true);
         TextView personajeTxt = rowView.findViewById(R.id.personaje_pj);
         TextView citaTxt = rowView.findViewById(R.id.cita_pj);
         ImageView imagePj= rowView.findViewById(R.id.image_pj);
         personajeTxt.setText(personajes.get(position).getCharacter());
         citaTxt.setText(personajes.get(position).getQuote());
-        Picasso.get().load(this.get(position).getImage()).resize(300,300).centerCrop().into(imagePj);
+        Picasso.get().load(this.personajes.get(position).getImage()).resize(300,300).centerCrop().into(imagePj);
         return rowView;
-
+        //jhofasfafg
     }
+
+
 }
